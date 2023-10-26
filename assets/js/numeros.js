c247d24c15beddf864d5a840bad956bdeb9ecadc
KUGUE <martin-kugue@hotmail.com>
2023-10-26 15:02:08 -0700

    Voces y vocales

--- assets/js/numeros.js
+++ assets/js/numeros.js
@@ -26,7 +26,6 @@ botonDer.addEventListener("click", function () {
 });
 
 // lector de numeros
-var voz = new SpeechSynthesisUtterance();
 async function speech() {
   var text = document.querySelector(".edadTitulo").textContent;
   if (text !== "") {
@@ -35,16 +34,31 @@ async function speech() {
   }
 }
 
+var voz = new SpeechSynthesisUtterance();
 voz.lang = "es-ES";
 voz.volume = 1;
+
+// Definir la voz que deseas utilizar
+var voiceToUse = null;
+
 var timer = setInterval(function () {
   var voices = speechSynthesis.getVoices();
   if (voices.length !== 0) {
-    voz.voice = voices[7];
-    voz.voiceURI = voices[7].voiceURI;
-    clearInterval(timer);
+    // Busca la voz que deseas utilizar, por ejemplo, por su nombre.
+    for (var i = 0; i < voices.length; i++) {
+      if (voices[i].name === "Google español") {
+        voiceToUse = voices[i];
+        break;
+      }
+    }
+
+    // Si se encontró la voz, configúrala
+    if (voiceToUse) {
+      voz.voice = voiceToUse;
+      voz.voiceURI = voiceToUse.voiceURI;
+      clearInterval(timer);
+    }
   }
-  console.log(voices)
 }, 100);
 
 setTimeout(function () {
@@ -53,10 +67,9 @@ setTimeout(function () {
 
 var vozEnReproduccion = false;
 function speechh(vocal) {
-
   if (!vozEnReproduccion) {
     vozEnReproduccion = true;
-    var text = vocal;
+    var text =  vocal;
     if (text !== "") {
       voz.text = text;
       window.speechSynthesis.speak(voz);
@@ -67,6 +80,7 @@ function speechh(vocal) {
     };
   }
 }
+
 
 // mostrar escrito
 
--- assets/js/voces.js
+++ assets/js/voces.js
@@ -1,40 +1,54 @@
 async function speech() {
-    var text = document.querySelector(".edadTitulo").textContent;
+  var text = document.querySelector(".edadTitulo").textContent;
+  if (text !== "") {
+    voz.text = text;
+    window.speechSynthesis.speak(voz);
+  }
+}
+
+var voz = new SpeechSynthesisUtterance();
+voz.lang = "es-ES";
+voz.volume = 1;
+
+// Definir la voz que deseas utilizar
+var voiceToUse = null;
+
+var timer = setInterval(function () {
+  var voices = speechSynthesis.getVoices();
+  if (voices.length !== 0) {
+    // Busca la voz que deseas utilizar, por ejemplo, por su nombre.
+    for (var i = 0; i < voices.length; i++) {
+      if (voices[i].name === "Google español") {
+        voiceToUse = voices[i];
+        break;
+      }
+    }
+
+    // Si se encontró la voz, configúrala
+    if (voiceToUse) {
+      voz.voice = voiceToUse;
+      voz.voiceURI = voiceToUse.voiceURI;
+      clearInterval(timer);
+    }
+  }
+}, 100);
+
+setTimeout(function () {
+  speech();
+}, 100);
+
+var vozEnReproduccion = false;
+function speechh(vocal) {
+  if (!vozEnReproduccion) {
+    vozEnReproduccion = true;
+    var text = "vocal, " + vocal;
     if (text !== "") {
       voz.text = text;
       window.speechSynthesis.speak(voz);
     }
+
+    voz.onend = function () {
+      vozEnReproduccion = false;
+    };
   }
-  var voz = new SpeechSynthesisUtterance();
-  voz.lang = "es-ES";
-  voz.volume = 1;
-  var timer = setInterval(function () {
-    var voices = speechSynthesis.getVoices();
-    if (voices.length !== 0) {
-      voz.voice = voices[7];
-      voz.voiceURI = voices[7].voiceURI;
-      clearInterval(timer);
-    }
-    console.log(voices)
-  }, 100);
-
-  setTimeout(function () {
-    speech();
-  }, 100);
-
-  var vozEnReproduccion = false; 
-  function speechh(vocal) {
-
-    if (!vozEnReproduccion) {
-      vozEnReproduccion = true; 
-      var text = "vocal, " + vocal;
-      if (text !== "") {
-        voz.text = text;
-        window.speechSynthesis.speak(voz);
-      }
-
-      voz.onend = function () {
-        vozEnReproduccion = false; 
-      };
-    }
-  }
+}
