fd135050d78d22ed24f12ded38d20371eed2456d
KUGUE <martin-kugue@hotmail.com>
2023-11-02 03:54:28 -0700

    Rutas 4-5

--- assets/js/play.js
+++ assets/js/play.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   function toggleAudioAndImage() {
     if (audio.paused) {
       audio.play();
-      playImage.src = '/assets/img/stop.png';
+      playImage.src = '../../../assets/img/stop.png';
     } else {
       audio.pause();
       audio.currentTime = 0;  
-      playImage.src = '/assets/img/play.png';
+      playImage.src = '../../../assets/img/play.png';
     }
   }
   playImage.addEventListener('click', toggleAudioAndImage);
   audio.addEventListener('ended', () => {
-    playImage.src = '/assets/img/play.png';
+    playImage.src = '../../../assets/img/play.png';
   });
 });
